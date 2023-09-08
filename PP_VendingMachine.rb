73e17082028d11d2f5a89f5ddadec9c47d6b9024
shuto kikai <yysskikai04@icloud.com>
2023-09-08 16:19:02 +0900

    コメントの削除

--- PP_VendingMachine.rb
+++ PP_VendingMachine.rb
@@ -1,35 +1,22 @@
 class VendingMachine
-  # ステップ０　お金の投入と払い戻しの例コード
-  # ステップ１　扱えないお金の例コード
 
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   MONEY = [10, 50, 100, 500, 1000].freeze
   
-  # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
-    # 最初の自動販売機に入っている金額は0円
     @slot_money = 0
     @juice = {name: "コーラ", price: 120, stock: 5}
     @sales_money = 0
   end
 
-
-    # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
-  # 投入は複数回できる。
   def slot_money
     while true#ユーザーが購入処理or返金に移るまで繰り返す
       puts "お金を投入してください"
       money = gets.chomp.to_i
-          # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
-      # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
-      # return money unless MONEY.include?(money)
       if MONEY.include?(money)
-        # 自動販売機にお金を入れる
-          @slot_money += money
+        @slot_money += money
       else
         puts "10円玉、50円玉、100円玉、500円玉、1000円札以外は使用できません。#{money}円を返金します。"
       end
-      
       puts "現在の金額は#{@slot_money}円です。投入金額は以上ですか？"
       puts "以上です：1"
       puts "追加する：2"
@@ -61,16 +48,13 @@ class VendingMachine
           elsif num == 3
             puts "#{@slot_money}円を返金します"
             exit
-          else #1~3以外の入力を処理すると49行目に戻るためelseの中は記載不要
           end
         end
       end
     end
   end
 
-  # 投入金額の総計を取得できる。
   def current_slot_money
-    # 自動販売機に入っているお金を表示する
     puts "合計金額は#{@slot_money}円です。"
     puts "購入に移る：1"
     puts "払い戻す：2"
@@ -138,17 +122,3 @@ vm = VendingMachine.new
 vm.slot_money
 vm.current_slot_money
 vm.buy
-
-  # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
-  def return_money
-    # 返すお金の金額を表示する
-    
-      puts @slot_money
-    # 自動販売機に入っているお金を0円に戻す
-    @slot_money = 0
-  end
-
-  juice = {name: "コーラ", price: 120, stock: 5} 
-
-  #次回やること
-  #飲み物の種類を増やす
